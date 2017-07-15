015ff4bff909ff8f6221030f7eb4991b3750650b
Jules Fowler <jfowler@stsci.edu>
2017-07-15 11:28:31 -0500

    Added two tests for non-integers.

--- sprint_tutorial/tests/test_compute.py
+++ sprint_tutorial/tests/test_compute.py
@@ -11,3 +11,13 @@ def test_my_sum1():
 
 def test_my_sum2():
     assert_equal(my_sum(1, -1), 0)
+
+
+def nonint_my_sum_test_1():
+    """A test of my sum for non-integers."""
+    assert_equal(my_sum(float(1), float(1)), float(2))
+
+
+def nonint_my_sum_test_2():
+    """Another test for nonints."""
+    assert_equal(my_sum(0.5, 0.5), float(1))
